f418880670af585aed05a0621178febc01505abb
QingRang <970753519@qq.com>
2019-09-27 15:26:15 +0800

    新增 穿山甲原生SDK 接入

---
+++ TTADAndroidSDKAccess/SDKTest/Assets/Scenes/Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+
+    private AndroidJavaClass jc;
+    private AndroidJavaObject jo;
+    public Button btnLoadVerticalAD;
+    public Button btnShowAD;
+
+    public void Awake() {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            // 获取 Unity 的 MainActivity 的固定写法
+            jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+#endif
+        btnLoadVerticalAD.onClick.AddListener(VerticalBtnClick);
+        btnShowAD.onClick.AddListener(ShowVedioBtnClick);
+    }
+
+    /// <summary>
+    /// 加载竖屏广告
+    /// </summary>
+    public void VerticalBtnClick() {
+        if (jo != null) {
+            jo.Call("Init");
+            jo.Call("LoadVerticalAD");
+        }
+    }
+
+    /// <summary>
+    /// 显示广告视频内容
+    /// </summary>
+    public void ShowVedioBtnClick() {
+        if (jo != null) {
+            jo.Call("ShowVedio");
+        }
+    }
+}
